2226342253633778bc4018eee56ba9889ba6ac4e
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-21 11:14:53 +0530

    first commit

---
+++ backend/Server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require("dotenv").config()
+const PORT = process.env.PORT | 5000;
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.send('Hello Prasadi..!')
+})
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log(`Connected to theMongoDB`)
+    app.listen(PORT, ()=>{
+        console.log(`NodeApi is running on PORT ${PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(error)
+})
